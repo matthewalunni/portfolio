817826bc4ac9673d257b38295823af3a028c3106
matthewalunni <matthew.alunni@gmail.com>
2021-12-24 11:10:30 -0500

    Style fix.

--- pages/components/projects.js
+++ pages/components/projects.js
@@ -54,7 +54,7 @@ export default function Projects() {
             <div className="max-w-7xl mx-auto">
                 <div className="relative z-10 pb-8 bg-gray-800 sm:pb-16 md:pb-20 lg:w-full lg:pb-28 xl:pb-32">
 
-                    <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+                    <main className="mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-10 lg:xl:mt-12 lg:px-8 ">
                         <div className="content-center sm:text-center lg:text-center">
                             <h1 className="text-4xl tracking-tight font-extrabold text-white sm:text-5xl md:text-6xl">
                                 <span className="block xl:inline">Check out my personal projects.</span>
