ba54eaf3f924d175fbab21a554a8ae05df345773
Matthew Alunni <matthew.alunni@gmail.com>
2026-01-11 20:40:55 -0500

    Fix base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://matthewalunni.github.io/portfolio/",
-  base: "/portfolio",
+  site: "https://matthewalunni.github.io",
+  base: "/portfolio/",
   output: "static",
 });
